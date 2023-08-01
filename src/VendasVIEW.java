e354b90dd1e61d1e2fb484b522e6104349473d82
Erica Chaves <138732799+ericachaves@users.noreply.github.com>
2023-07-31 21:59:59 -0300

    Botão consultar vendas

--- src/VendasVIEW.java
+++ src/VendasVIEW.java
@@ -7,12 +7,12 @@
  *
  * @author vinicius
  */
-public class Vendas extends javax.swing.JFrame {
+public class VendasVIEW extends javax.swing.JFrame {
 
     /**
      * Creates new form Vendas
      */
-    public Vendas() {
+    public VendasVIEW() {
         initComponents();
     }
 
@@ -103,20 +103,21 @@ public class Vendas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vendas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VendasVIEW.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Vendas().setVisible(true);
+                new VendasVIEW().setVisible(true);
             }
         });
     }
